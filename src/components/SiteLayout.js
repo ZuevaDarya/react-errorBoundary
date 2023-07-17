27735bb363d9c5837ad0df93bfcc70bae7359898
ZuevaDarya <zuevadasha55@gmail.com>
2023-07-17 17:13:22 +0300

    import style in SiteLayout.js

--- src/components/SiteLayout.js
+++ src/components/SiteLayout.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../styles/siteLayout.css';
 
 export default function SiteLayout({ children, menu = c => null }) {
   return (
     <div className="site-container">
-      <div>{menu}</div>
-      <div>{children}</div>
+      <div className="site-container__menu">{menu}</div>
+      <div className="site-container__childern">{children}</div>
     </div>
   );
 }
