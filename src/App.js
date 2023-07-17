da0935ce52b2963792a7a2b3bc2d38c01137a11f
ZuevaDarya <zuevadasha55@gmail.com>
2023-07-17 16:24:38 +0300

    change App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
+import SiteLayout from './components/SiteLayout';
 import './App.css';
 
 export default function App() {
   return (
-    <>
-    </>
+    <SiteLayout menu={<p>Menu</p>}>
+      <>
+        <h1>Contents</h1>
+        <p>This is a main part of the example layuot</p>
+      </>
+    </SiteLayout>
   );
 }
 
